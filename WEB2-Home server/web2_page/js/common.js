87d964ade49b49214f16260b795d882755c4b5a8
developer-do <dev-do@naver.com>
2019-02-08 23:45:12 +0900

    stopgit add -A

--- WEB2-Home server/web2_page/js/common.js
+++ WEB2-Home server/web2_page/js/common.js
@@ -20,42 +20,49 @@ titleOutput(document.title);
 
 /* menu list */
 function arrListOutput() {
+  let list = `<ul>`;
+  let menu = document.querySelector(".menu");
+  
   for (let i = 0; i < menuList.length; i++) {
-    document.write(`<li class="list${i}"><a href="Home-server-${i + 1}.html">${listFix + menuList[i]}</a></li>`);
+    list += `<li class="list${i}"><a href="Home-server-${i + 1}.html">${listFix + menuList[i]}</a></li>`;
   }
+  list += `</ul>`;
+  list += `<p class="clickHide">마우스 휠을 건드리시거나 이곳을 클릭하시면 메뉴바가 사라집니다.</p>`;
+  menu.innerHTML = list;
 }
 arrListOutput();
 
 
 
-
+/* fixedSpan 메뉴 클릭할 시에  */
 function menuClick() {
   let fixedSpan = document.getElementById("fixedSpan");
   fixedSpan.addEventListener("click", menuShow);
 }
-
 function menuShow() {
   let menu = document.querySelector(".menu");
   menu.classList.remove("disNone");
   menu.classList.add("disBlock");
 }
+menuClick();
 
 function menuHide() {
+  let clickHide = document.querySelector(".clickHide");
+  clickHide.addEventListener("click", hideOpt);
   let body = document.querySelector("body");
   body.addEventListener("mousewheel", wheelHide);
 }
-
-menuHide();
-
+/* wheel을 올리거나 내리면 menu 숨기기 */
 function wheelHide(e) {
   if (e.wheelDelta !== 0 || e.wheelDeltaY === -120 || e.wheelDeltaY === 120) {
-    let menu = document.querySelector(".menu");
-    menu.classList.add("disNone");
-    menu.classList.remove("disBlock");
+    hideOpt();
   }
 }
 
-menuClick();
+function hideOpt() {
+  let menu = document.querySelector(".menu");
+  menu.classList.add("disNone");
+  menu.classList.remove("disBlock");
+}
 
-
-console.log(fixedSpan);
+menuHide();
